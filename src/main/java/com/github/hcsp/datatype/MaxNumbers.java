e144713efe948cd17ac1483c489d8af57ae1beb8
Bo Zhang <bo@gradle.com>
2019-06-02 09:32:32 +0800

    Initial commit

---
+++ src/main/java/com/github/hcsp/datatype/MaxNumbers.java
@@ -0,0 +1,47 @@
+package com.github.hcsp.datatype;
+public class MaxNumbers {
+    // byte类型能存储的最大值是？
+    // The maximum value a byte type can store
+    public static byte maxByte = ?;
+
+    // short类型能存储的最大值是？
+    // The maximum value a short type can store
+    public static short maxShort = ?;
+
+    // int类型能存储的最大值是？
+    // The maximum value an int type can store
+    public static int maxInt = ?;
+
+    // long类型能存储的最大值是？
+    // The maximum value an long type can store
+    public static long maxLong = ?;
+
+    // float类型能存储的最大值是？
+    // The maximum value an float type can store
+    public static float maxFloat = ?;
+
+    // double类型能存储的最大值是？
+    // The maximum value an double type can store
+    public static Long maxDouble = ?;
+
+    // char类型能存储的最大值是？
+    // The maximum value an char type can store
+    public static char maxChar = ?;
+
+    public static void main(String[] args) {
+        System.out.println("maxByte=" + maxByte);
+        System.out.println("maxByte+1=" + (maxByte + 1));
+        System.out.println("maxShort=" + maxShort);
+        System.out.println("maxShort+1=" + (maxShort + 1);
+        System.out.println("maxInt=" + maxInt);
+        System.out.println("maxInt+1=" + (maxInt + 1));
+        System.out.println("maxLong=" + maxLong);
+        System.out.println("maxLong+1=" + (maxLong + 1));
+        System.out.println("maxFloat=" + maxFloat);
+        System.out.println("maxFloat+1=" + (maxFloat + 1));
+        System.out.println("maxDouble=" + maxDouble);
+        System.out.println("maxDouble+1=" + (maxDouble + 1));
+        System.out.println("maxChar=" + maxChar);
+        System.out.println("maxChar+1=" + (maxChar + 1));
+    }
+}
---
+++ src/test/java/com/github/hcsp/datatype/MaxNumbersTest.java
@@ -0,0 +1,18 @@
+package com.github.hcsp.datatype;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MaxNumbersTest {
+    @Test
+    public void test() {
+        assertEquals(Byte.MAX_VALUE, MaxNumbers.maxByte);
+        assertEquals(Short.MAX_VALUE, MaxNumbers.maxShort);
+        assertEquals(Integer.MAX_VALUE, MaxNumbers.maxInt);
+        assertEquals(Long.MAX_VALUE, MaxNumbers.maxLong);
+        assertEquals(Float.MAX_VALUE, MaxNumbers.maxFloat);
+        assertEquals(Double.MAX_VALUE, MaxNumbers.maxDouble);
+        assertEquals(Charater.MAX_VALUE, MaxNumbers.maxChar);
+    }
+}
