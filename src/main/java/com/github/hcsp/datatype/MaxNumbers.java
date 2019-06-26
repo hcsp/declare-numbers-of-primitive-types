ec6c785a5f2992175848544da88b7739e70cd5d8
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-06-26 21:31:13 +0800

    Update MaxNumbers.java

--- src/main/java/com/github/hcsp/datatype/MaxNumbers.java
+++ src/main/java/com/github/hcsp/datatype/MaxNumbers.java
@@ -18,11 +18,11 @@ public class MaxNumbers {
 
     // float类型能存储的最大值是？
     // The maximum value an float type can store
-    public static float maxFloat = 0x7ffffffff;
+    public static float maxFloat = 0x7fffffff;
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble = 0x7ffffffffffffffffd;
+    public static double maxDouble = 0x7ffffffffffffffff;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
