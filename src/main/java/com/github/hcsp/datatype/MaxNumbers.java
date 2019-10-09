a41040eb6bedc5c44afc3316c8f76ee79aa61238
ws1603 <18388866683@163.com>
2019-10-09 16:12:12 +0800

    在MaxNumbers这个类中声明了不同类型的指定数据

--- src/main/java/com/github/hcsp/datatype/MaxNumbers.java
+++ src/main/java/com/github/hcsp/datatype/MaxNumbers.java
@@ -1,12 +1,13 @@
 package com.github.hcsp.datatype;
+
 public class MaxNumbers {
     // byte类型能存储的最大值是？
     // The maximum value a byte type can store
-    public static byte maxByte =127;
+    public static byte maxByte = 127;
 
     // short类型能存储的最大值是？
     // The maximum value a short type can store
-    public static short maxShort =32767;
+    public static short maxShort = 32767;
 
     // int类型能存储的最大值是？
     // The maximum value an int type can store
@@ -14,19 +15,19 @@ public class MaxNumbers {
 
     // long类型能存储的最大值是？
     // The maximum value an long type can store
-    public static long maxLong =0x7fffffffffffffffL;
+    public static long maxLong = 0x7fffffffffffffffL;
 
     // float类型能存储的最大值是？
     // The maximum value an float type can store
-    public static float maxFloat =0x1.fffffeP+127f;
+    public static float maxFloat = 0x1.fffffeP+127f;
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble =0x1.fffffffffffffP+1023;
+    public static double maxDouble = 0x1.fffffffffffffP+1023;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
-    public static char maxChar ='\uFFFF';
+    public static char maxChar = '\uFFFF';
 
     public static void main(String[] args) {
         System.out.println("maxByte=" + maxByte);
@@ -34,7 +35,7 @@ public class MaxNumbers {
         System.out.println("maxShort=" + maxShort);
         System.out.println("maxShort+1=" + (maxShort + 1));
         System.out.println("maxInt=" + maxInt);
-        System.out.println("maxInt+1=" + (maxInt +1));
+        System.out.println("maxInt+1=" + (maxInt + 1));
         System.out.println("maxLong=" + maxLong);
         System.out.println("maxLong+1=" + (maxLong + 1));
         System.out.println("maxFloat=" + maxFloat);
