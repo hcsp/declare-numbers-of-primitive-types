23027a78db6dfa10f94aaaedf39fa384a108f4b5
ws1603 <18388866683@163.com>
2019-10-09 16:07:31 +0800

    在MaxNumbers这个类中声明了不同类型的指定数据

--- src/main/java/com/github/hcsp/datatype/MaxNumbers.java
+++ src/main/java/com/github/hcsp/datatype/MaxNumbers.java
@@ -2,31 +2,31 @@ package com.github.hcsp.datatype;
 public class MaxNumbers {
     // byte类型能存储的最大值是？
     // The maximum value a byte type can store
-    public static byte maxByte = ?;
+    public static byte maxByte =127;
 
     // short类型能存储的最大值是？
     // The maximum value a short type can store
-    public static short maxShort = ?;
+    public static short maxShort =32767;
 
     // int类型能存储的最大值是？
     // The maximum value an int type can store
-    public static int maxInt = ?;
+    public static int maxInt = 0x7fffffff;
 
     // long类型能存储的最大值是？
     // The maximum value an long type can store
-    public static long maxLong = ?;
+    public static long maxLong =0x7fffffffffffffffL;
 
     // float类型能存储的最大值是？
     // The maximum value an float type can store
-    public static float maxFloat = ?;
+    public static float maxFloat =0x1.fffffeP+127f;
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble = ?;
+    public static double maxDouble =3.4e+21;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
-    public static char maxChar = ?;
+    public static char maxChar ='\uFFFF';
 
     public static void main(String[] args) {
         System.out.println("maxByte=" + maxByte);
@@ -34,7 +34,7 @@ public class MaxNumbers {
         System.out.println("maxShort=" + maxShort);
         System.out.println("maxShort+1=" + (maxShort + 1));
         System.out.println("maxInt=" + maxInt);
-        System.out.println("maxInt+1=" + (maxInt + 1));
+        System.out.println("maxInt+1=" + (maxInt +1));
         System.out.println("maxLong=" + maxLong);
         System.out.println("maxLong+1=" + (maxLong + 1));
         System.out.println("maxFloat=" + maxFloat);
