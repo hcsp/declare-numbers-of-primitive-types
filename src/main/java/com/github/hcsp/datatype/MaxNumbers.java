a84c22107c6e800107880390aad9138aff54004d
sunp <sunp@SH-CF12-DEV.rnd.aspentech.com>
2019-10-17 19:53:17 -0700

    Update the MaxNumbers.java

--- src/main/java/com/github/hcsp/datatype/MaxNumbers.java
+++ src/main/java/com/github/hcsp/datatype/MaxNumbers.java
@@ -4,6 +4,7 @@ public class MaxNumbers {
     // The maximum value a byte type can store
     public static byte maxByte = Byte.MAX_VALUE;
 
+
     // short类型能存储的最大值是？
     // The maximum value a short type can store
     public static short maxShort = Short.MAX_VALUE;
