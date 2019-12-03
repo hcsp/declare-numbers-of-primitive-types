3af24702661ca180798a21c0427aaae892b4eefd
llaobin0207 <llaobin0207@163.com>
2019-12-04 02:04:31 +0800

    Java的数据类型练习：声明原生数据类型的指定数字

--- src/main/java/com/github/hcsp/datatype/MaxNumbers.java
+++ src/main/java/com/github/hcsp/datatype/MaxNumbers.java
@@ -14,19 +14,19 @@ public class MaxNumbers {
 
     // long类型能存储的最大值是？
     // The maximum value an long type can store
-    public static long maxLong = (long) (Math.pow(2,63)-1);
+    public static long maxLong = 9223372036854775807l;
 
     // float类型能存储的最大值是？
     // The maximum value an float type can store
-    public static float maxFloat = (float) (3.4028*Math.pow(10,38));
+    public static float maxFloat = 3.4028235E38f;
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble =1.7977*Math.pow(10,308);
+    public static double maxDouble =1.7976931348623157E308d;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
-    public static char maxChar = 65535;
+    public static char maxChar = '\uFFFF';
 
     public static void main(String[] args) {
         System.out.println("maxByte=" + maxByte);
